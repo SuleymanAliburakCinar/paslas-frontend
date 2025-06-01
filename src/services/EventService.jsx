649327062ecaa4d328f8c2080b983c6ac11227c5
Suley <sabc4129@gmail.com>
2025-06-01 13:35:09 +0300

    Lobby Page init
    Lobby page initialized with User list, Event details and event list.
    Related api added.

---
+++ src/services/EventService.jsx
@@ -0,0 +1,26 @@
+import api from '../lib/axios';
+
+const getAllEvents = (lobbyId) => {
+  return api.get(`/api/event/getAll/${lobbyId}`);
+}
+
+const getParticipant = (eventId) => {
+  return api.get(`/api/event/participations/${eventId}`);
+}
+
+const applyEvent = (eventId) => {
+  return api.get(`/api/event/apply/${eventId}`);
+}
+
+const leaveEvent = (eventId) => {
+  return api.get(`/api/event/leave/${eventId}`);
+}
+
+const lobbyService = {
+  getAllEvents,
+  getParticipant,
+  applyEvent,
+  leaveEvent,
+};
+
+export default lobbyService;
